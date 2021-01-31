d9a4d190369d8de25735a27050b509abfe0988f5
NovaWeb Chile <webmaster@novaweb.cl>
2021-01-31 12:58:32 -0300

    Update Cambiar el texto del botón Añadir al carrito en WooCommerce en un producto individual.php
    Cambia el texto del botón Añadir al carrito en WooCommerce en un producto individual: versiones menores a 2.1

--- Cambiar el texto del botón Añadir al carrito en WooCommerce en un producto individual.php
+++ Cambiar el texto del botón Añadir al carrito en WooCommerce en un producto individual.php
@@ -1,5 +1,5 @@
 <?php
-//* NO incluyas la etiqueta de apertura
+//* NO incluyas la etiqueta de aperturaa o no funcionara
  
 //* Cambia el texto del botón Añadir al carrito en WooCommerce en un producto individual: versiones menores a 2.1
 add_filter( 'add_to_cart_text', 'woo_custom_cart_button_text' );
